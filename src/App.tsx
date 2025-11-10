491af9a777a0055999278fefa48a12bbffa80f4e
tajpouria <46822325+tajpouria@users.noreply.github.com>
2025-11-10 16:43:11 +0100

    feat: add Molentje Privacy Policy page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import MolentjePrivacyPolicy from "./pages/MolentjePrivacyPolicy";
 
 
 const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/molentje/privacy-policy" element={<MolentjePrivacyPolicy />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
